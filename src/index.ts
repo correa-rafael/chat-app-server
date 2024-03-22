ecddaf0877f0b821886a0b17234efcbce36d8017
Rafael Correa <rafaelcorrea15@yahoo.com.br>
2024-03-22 21:42:47 +0100

    timeout removed

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ const io = new Server(server, {
     origin: '*',
     methods: ['GET', 'POST'],
   },
-  pingTimeout: 900000, // tempo limite de ping em milissegundos (aumente se necessário)
 });
 
 const port = process.env.PORT || 4000;
